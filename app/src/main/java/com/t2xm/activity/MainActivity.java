201a35c31e09d793067241979cd03db814da74de
gq <swe1904862@xmu.edu.my>
2021-12-20 18:01:56 +0800

    add bottom navigation bar

---
+++ app/src/main/java/com/t2xm/activity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.t2xm.activity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.t2xm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        ((BottomNavigationView)findViewById(R.id.bottom_nav)).setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return false;
+            }
+        });
+    }
+}
--- app/src/main/java/com/t2xm/activity/SignUpActivity.java
+++ app/src/main/java/com/t2xm/activity/SignUpActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.t2xm.R;
+
 public class SignUpActivity extends AppCompatActivity {
 
     @Override
