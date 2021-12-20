89604d4ef969977bd3b5a062bb37acf1e08b71ab
gq <swe1904862@xmu.edu.my>
2021-12-20 18:34:18 +0800

    add bottom navigation function

--- app/src/main/java/com/t2xm/activity/MainActivity.java
+++ app/src/main/java/com/t2xm/activity/MainActivity.java
@@ -1,25 +1,63 @@
 package com.t2xm.activity;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.t2xm.R;
+import com.t2xm.fragment.DoFragment;
+import com.t2xm.fragment.EatFragment;
+import com.t2xm.fragment.HomeFragment;
+import com.t2xm.fragment.ProfileFragment;
+import com.t2xm.fragment.StayFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final HomeFragment HOME_FRAGMENT = new HomeFragment();
+    private final DoFragment DO_FRAGMENT = new DoFragment();
+    private final StayFragment STAY_FRAGMENT = new StayFragment();
+    private final EatFragment EAT_FRAGMENT = new EatFragment();
+    private final ProfileFragment PROFILE_FRAGMENT = new ProfileFragment();
+
+    private Fragment activeFragment = HOME_FRAGMENT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((BottomNavigationView)findViewById(R.id.bottom_nav)).setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                return false;
+                Fragment selectedFragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.bn_home:
+                        selectedFragment = HOME_FRAGMENT;
+                        break;
+                    case R.id.bn_do:
+                        selectedFragment = DO_FRAGMENT;
+                        break;
+                    case R.id.bn_stay:
+                        selectedFragment = STAY_FRAGMENT;
+                        break;
+                    case R.id.bn_eat:
+                        selectedFragment = EAT_FRAGMENT;
+                        break;
+                    case R.id.bn_profile:
+                        selectedFragment = PROFILE_FRAGMENT;
+                        break;
+                    default:
+                        return false;
+                }
+                getSupportFragmentManager().beginTransaction().hide(activeFragment).show(selectedFragment).commit();
+               activeFragment = selectedFragment;
+                return true;
             }
         });
     }
