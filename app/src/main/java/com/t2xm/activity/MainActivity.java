e21e5e46e6bf401a3205a5cfe7cfc632df689b52
gq <swe1904862@xmu.edu.my>
2021-12-20 18:05:11 +0800

    rename activity_home.xml -> activity_main.xml

--- app/src/main/java/com/t2xm/activity/MainActivity.java
+++ app/src/main/java/com/t2xm/activity/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
 
         ((BottomNavigationView)findViewById(R.id.bottom_nav)).setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
