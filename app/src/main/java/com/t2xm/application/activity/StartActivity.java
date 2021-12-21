3540ff189698139702a2cd4b6aa3c47dd06c69af
gq <swe1904862@xmu.edu.my>
2021-12-21 15:12:34 +0800

    change default activity to StartActivity

--- app/src/main/java/com/t2xm/application/activity/StartActivity.java
+++ app/src/main/java/com/t2xm/application/activity/StartActivity.java
@@ -1,16 +1,29 @@
 package com.t2xm.application.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.t2xm.R;
 
 public class StartActivity extends AppCompatActivity {
 
+    //TODO change delay time
+    private final long DELAY_TIME = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //TODO change activity to start to Login Activity
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }, DELAY_TIME);
     }
 }
