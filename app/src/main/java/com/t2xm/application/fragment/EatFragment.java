e669a4cfa66ad1667af43a4a51a1731550b4f697
gq <swe1904862@xmu.edu.my>
2021-12-21 17:44:37 +0800

    remove fragment_stay.xml and fragment_eat.xml

--- app/src/main/java/com/t2xm/application/fragment/EatFragment.java
+++ app/src/main/java/com/t2xm/application/fragment/EatFragment.java
@@ -15,6 +15,6 @@ public class EatFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_eat, container, false);
+        return inflater.inflate(R.layout.fragment_do, container, false);
     }
 }
--- app/src/main/java/com/t2xm/application/fragment/StayFragment.java
+++ app/src/main/java/com/t2xm/application/fragment/StayFragment.java
@@ -15,6 +15,6 @@ public class StayFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_stay, container, false);
+        return inflater.inflate(R.layout.fragment_do, container, false);
     }
 }
