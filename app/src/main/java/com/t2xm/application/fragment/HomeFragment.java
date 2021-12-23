cf4c8bbe960515a1d83bb96063b293c2c99c4ed9
gq <swe1904862@xmu.edu.my>
2021-12-23 19:11:19 +0800

    create ListItemAdapter

--- app/src/main/java/com/t2xm/application/fragment/HomeFragment.java
+++ app/src/main/java/com/t2xm/application/fragment/HomeFragment.java
@@ -8,13 +8,31 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.t2xm.R;
+import com.t2xm.dao.ItemDao;
+import com.t2xm.entity.Item;
+import com.t2xm.utils.adapter.ListItemAdapter;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        RecyclerView rv = view.findViewById(R.id.rv_latest_reviews);
+       List<Item> itemList = ItemDao.getItemListByCategory(1) ;
+        ListItemAdapter listItemAdapter = new ListItemAdapter(getContext(), itemList);
+        rv.setAdapter(listItemAdapter);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
---
+++ app/src/main/java/com/t2xm/utils/adapter/ListItemAdapter.java
@@ -0,0 +1,68 @@
+package com.t2xm.utils.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.t2xm.R;
+import com.t2xm.entity.Item;
+
+import java.util.List;
+
+
+public class ListItemAdapter extends RecyclerView.Adapter<ListItemAdapter.ViewHolder> {
+
+   private LayoutInflater inflater;
+    private Context context;
+    private List<Item> itemList;
+
+
+    public ListItemAdapter(Context context, List<Item> itemList) {
+        this.context = context;
+        this.itemList = itemList;
+        this.inflater = LayoutInflater.from(this.context);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, viewGroup, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, final int position) {
+        Item item = getItemByPosition(position);
+        viewHolder.iv_itemImage.setImageResource(this.context.getResources().getIdentifier(item.image, "drawable", context.getPackageName()));
+        viewHolder.tv_itemName.setText(item.itemName);
+        viewHolder.tv_itemContent.setText(item.description);
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemList.size();
+    }
+
+    private Item getItemByPosition(int position) {
+        return itemList.get(position);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public ImageView iv_itemImage;
+        public TextView tv_itemName;
+        public TextView tv_itemRating;
+        public TextView tv_itemContent;
+
+        public ViewHolder(View view) {
+            super(view);
+            iv_itemImage = view.findViewById(R.id.iv_item_image);
+            tv_itemName = view.findViewById(R.id.tv_item_name);
+            tv_itemRating = view.findViewById(R.id.tv_item_rating);
+            tv_itemContent = view.findViewById(R.id.tv_item_content);
+        }
+    }
+}
