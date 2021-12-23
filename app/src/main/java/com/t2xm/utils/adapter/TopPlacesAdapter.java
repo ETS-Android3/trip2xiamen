de7d8b4b0f664e21c44aec607e1541af5df14401
gq <swe1904862@xmu.edu.my>
2021-12-23 23:00:42 +0800

    set onclick listener for top places

--- app/src/main/java/com/t2xm/utils/adapter/TopPlacesAdapter.java
+++ app/src/main/java/com/t2xm/utils/adapter/TopPlacesAdapter.java
@@ -2,15 +2,18 @@ package com.t2xm.utils.adapter;
 
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.t2xm.R;
+import com.t2xm.application.activity.DetailsActivity;
 import com.t2xm.entity.Item;
 import com.t2xm.utils.JsonUtil;
 
@@ -38,13 +41,27 @@ public class TopPlacesAdapter extends RecyclerView.Adapter<TopPlacesAdapter.View
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, final int position) {
         Item item = getItemByPosition(position);
+
+        //setup item image
         try {
             String firstImageName = (String) JsonUtil.mapJsonToObject(item.image, List.class).get(0);
             viewHolder.iv_itemImage.setImageResource(this.context.getResources().getIdentifier(firstImageName, "drawable", context.getPackageName()));
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        //setup item name
         viewHolder.tv_itemName.setText(item.itemName);
+
+        //setup click listener
+        viewHolder.rl_container.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailsActivity.class);
+                intent.putExtra("itemId", item.itemId);
+                context.startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -57,12 +74,13 @@ public class TopPlacesAdapter extends RecyclerView.Adapter<TopPlacesAdapter.View
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        public RelativeLayout rl_container;
         public ImageView iv_itemImage;
         public TextView tv_itemName;
 
-
         public ViewHolder(View view) {
             super(view);
+            rl_container = view.findViewById(R.id.rl_list_container);
             iv_itemImage = view.findViewById(R.id.iv_item_image);
             tv_itemName = view.findViewById(R.id.tv_item_name);
         }
