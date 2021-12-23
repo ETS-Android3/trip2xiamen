6ee5a3b23130e3cf7fc5afbe69ede2544513d3e1
gq <swe1904862@xmu.edu.my>
2021-12-24 00:16:15 +0800

    add DateUtil

---
+++ app/src/main/java/com/t2xm/utils/DateUtil.java
@@ -0,0 +1,20 @@
+package com.t2xm.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateUtil {
+    private static SimpleDateFormat dateTimeFormat = new SimpleDateFormat("dd/MM/YY HH:mm:ss");
+
+    public static String getCurrentDateTime() {
+        return dateTimeFormat.format(new Date().getTime());
+    }
+
+    public static String getDateTimeByTimestamp(Long timestamp) {
+        return dateTimeFormat.format(new Date(timestamp).getTime());
+    }
+
+    public static Long getCurrentTimestamp() {
+        return new Date().getTime();
+    }
+}
