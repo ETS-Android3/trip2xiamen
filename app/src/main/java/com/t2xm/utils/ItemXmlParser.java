df53b5db06ebf7f05ca47741eecdd34dfdd75bff
gq <swe1904862@xmu.edu.my>
2021-12-24 13:18:26 +0800

    fix xml parser

--- app/src/main/java/com/t2xm/utils/ItemXmlParser.java
+++ app/src/main/java/com/t2xm/utils/ItemXmlParser.java
@@ -1,53 +1,72 @@
 package com.t2xm.utils;
 
 import android.content.res.XmlResourceParser;
+import android.util.Xml;
 
 import com.t2xm.entity.Item;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ItemXmlParser {
-    public static List<Item> parseItems(XmlResourceParser parser) throws XmlPullParserException, IOException, IllegalAccessException, NoSuchFieldException {
+    public static List<Item> parseItems(XmlResourceParser parser) throws XmlPullParserException, IOException {
         List<Item> itemList = new ArrayList<>();
-        while (parser.next() != XmlPullParser.END_TAG) {
-            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                continue;
-            }
-            if (parser.getName().equals("Item")) {
-                Item item = new Item();
-                while (parser.next() != XmlPullParser.END_TAG) {
-                    if (parser.getEventType() != XmlPullParser.START_TAG) {
-                        continue;
+        int eventType = parser.getEventType();
+        Item item = null;
+        List<String> imageNameList = null;
+
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            switch (eventType) {
+                case XmlPullParser.START_DOCUMENT:
+                    break;
+                case XmlPullParser.START_TAG:
+                    if(parser.getName().equals("Items")) {
+                        break;
                     }
-                    String attr = parser.getName();
-                    if (attr.equals("images")) {
-                        List<String> imageNameList = new ArrayList<>();
-                        while (parser.next() != XmlPullParser.END_TAG) {
-                            if (parser.getEventType() != XmlPullParser.START_TAG) {
-                                continue;
-                            }
-                            if (parser.next() == XmlPullParser.TEXT) {
-                                imageNameList.add(parser.getText());
-                                parser.nextTag();
-                            }
-                        }
+                    else if(parser.getName().equals("Item")) {
+                        item = new Item();
+                    }
+                    else if(parser.getName().equals("itemName")) {
+                        item.itemName = parser.nextText() ;
+                    }
+                    else if(parser.getName().equals("category")) {
+                        item.category = Integer.valueOf(parser.nextText());
+                    }
+                    else if(parser.getName().equals("description")) {
+                        item.description = parser.nextText();
+                    }
+                    else if(parser.getName().equals("images")) {
+                        imageNameList = new ArrayList<>();
+                    }
+                    else if(parser.getName().equals("image")) {
+                        imageNameList.add(parser.nextText()) ;
+                    }
+                    else if(parser.getName().equals("longitude")) {
+                        item.longitude = Double.valueOf(parser.nextText());
+                    }
+                    else if(parser.getName().equals("latitude")) {
+                        item.latitude = Double.valueOf(parser.nextText());
+                    }
+                    break;
+                case XmlPullParser.END_TAG:
+                    if(parser.getName().equals("Item")) {
+                        itemList.add(item);
+                    }
+                    else if(parser.getName().equals("images")) {
                         item.image = JsonUtil.mapObjectToJson(imageNameList);
                     }
-                    if (parser.next() == XmlPullParser.TEXT) {
-                        Field field = item.getClass().getDeclaredField(attr);
-                        field.set(item, FieldCastUtil.castStringToType(parser.getText(), field.getType()));
-                        parser.nextTag();
-                    }
-                }
-                itemList.add(item);
+                    break;
             }
+            eventType = parser.next();
         }
+
         return itemList;
     }
 }
