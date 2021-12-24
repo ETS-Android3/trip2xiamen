52aa6b08938936732911af58b213e3e79708f003
gq <swe1904862@xmu.edu.my>
2021-12-24 13:36:20 +0800

    debug ReviewActivity.java: get objects on create

--- app/src/main/java/com/t2xm/application/activity/ReviewActivity.java
+++ app/src/main/java/com/t2xm/application/activity/ReviewActivity.java
@@ -1,8 +1,5 @@
 package com.t2xm.application.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,12 +8,14 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.t2xm.R;
 import com.t2xm.dao.ItemDao;
 import com.t2xm.entity.Item;
 import com.t2xm.utils.JsonUtil;
-
-import org.w3c.dom.Text;
+import com.t2xm.utils.ToastUtil;
 
 import java.util.List;
 
@@ -39,17 +38,31 @@ public class ReviewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
 
+        iv_itemImage = findViewById(R.id.iv_item_image);
+        tv_itemName = findViewById(R.id.tv_item_name);
+        tv_reviewCharacterCount = findViewById(R.id.tv_review_character_count);
+        et_reviewText = findViewById(R.id.et_review_text);
+        rv_uploadImage = findViewById(R.id.rv_reviews);
+        cb_recommend = findViewById(R.id.cb_recommend);
+        btn_submitReview = findViewById(R.id.btn_submit_review);
+
         itemId = getIntent().getIntExtra("itemId", 0);
-        if(itemId <= 0) {
+        if (itemId <= 0) {
             //TODO error
+            ToastUtil.createAndShowToast(getApplicationContext(), "Error");
+            onBackPressed();
         }
 
-        item = ItemDao.getItemByItemId(itemId) ;
-        if(item == null) {
+        item = ItemDao.getItemByItemId(itemId);
+        if (item == null) {
             //TODO error
+            ToastUtil.createAndShowToast(getApplicationContext(), "Error");
+            onBackPressed();
         }
         try {
-            iv_itemImage.setImageResource(getResources().getIdentifier(String.valueOf(JsonUtil.mapJsonToObject(item.image, List.class).get(0)), "drawable", getPackageName()));
+            String img = String.valueOf(JsonUtil.mapJsonToObject(item.image, List.class).get(0));
+            int resource = getResources().getIdentifier(img, "drawable", getPackageName());
+            iv_itemImage.setImageResource(resource);
         } catch (Exception e) {
             e.printStackTrace();
         }
