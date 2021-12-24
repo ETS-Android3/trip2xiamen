5af7d565994d5c4e0bffee3fdbfdcf61e280d6a0
gq <swe1904862@xmu.edu.my>
2021-12-25 01:30:04 +0800

    add change password and email activity layout and function

--- app/src/main/java/com/t2xm/application/activity/ChangeEmailActivity.java
+++ app/src/main/java/com/t2xm/application/activity/ChangeEmailActivity.java
@@ -1,16 +1,53 @@
 package com.t2xm.application.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.t2xm.R;
+import com.t2xm.utils.ValidationUtil;
 
 public class ChangeEmailActivity extends AppCompatActivity {
+
+    private EditText et_currentEmail;
+    private EditText et_newEmail;
+    private EditText et_confirmEmail;
+
+    private String currentEmail;
+    private String newEmail;
+    private String confirmEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_email);
+
+        et_currentEmail = findViewById(R.id.et_current_email);
+        et_newEmail = findViewById(R.id.et_new_email);
+        et_confirmEmail = findViewById(R.id.et_confirm_email);
+
+        findViewById(R.id.btn_confirm_change).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO change email
+
+                //TODO check whether email match with the database
+            }
+        });
+    }
+
+    private boolean validateEmail() {
+        if(ValidationUtil.validateEmail(newEmail)) {
+            return newEmail.equals(confirmEmail);
+        }
+        return false;
+    }
+
+    private void updateInputFields() {
+        currentEmail = String.valueOf(et_currentEmail.getText());
+        newEmail = String.valueOf(et_newEmail.getText());
+        confirmEmail = String.valueOf(et_confirmEmail.getText());
     }
 }
--- app/src/main/java/com/t2xm/application/activity/ChangePasswordActivity.java
+++ app/src/main/java/com/t2xm/application/activity/ChangePasswordActivity.java
@@ -1,16 +1,53 @@
 package com.t2xm.application.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.t2xm.R;
+import com.t2xm.utils.ValidationUtil;
 
 public class ChangePasswordActivity extends AppCompatActivity {
+
+    private EditText et_currentPassword;
+    private EditText et_newPassword;
+    private EditText et_confirmPassword;
+
+    private String currentPassword;
+    private String newPassword;
+    private String confirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        et_currentPassword = findViewById(R.id.et_current_password);
+        et_newPassword = findViewById(R.id.et_new_password);
+        et_confirmPassword = findViewById(R.id.et_new_password);
+
+        findViewById(R.id.btn_confirm_change).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO change password
+
+                //TODO check whether current password match with database
+            }
+        });
+    }
+
+    private boolean validatePassword() {
+        if(ValidationUtil.validatePassword(newPassword)) {
+            return newPassword.equals(confirmPassword);
+        }
+        return false;
+    }
+
+    private void updateInputFields() {
+        currentPassword = String.valueOf(et_currentPassword.getText());
+        newPassword = String.valueOf(et_newPassword.getText());
+        confirmPassword = String.valueOf(et_confirmPassword.getText());
     }
 }
