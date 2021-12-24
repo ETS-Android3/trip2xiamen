eee333fc8df695607bb63d46844d85e9d1dea580
gq <swe1904862@xmu.edu.my>
2021-12-24 17:53:55 +0800

    edit my favourite page

--- app/src/main/java/com/t2xm/application/activity/MyFavouriteActivity.java
+++ app/src/main/java/com/t2xm/application/activity/MyFavouriteActivity.java
@@ -1,16 +1,47 @@
 package com.t2xm.application.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.tabs.TabLayout;
 import com.t2xm.R;
 
 public class MyFavouriteActivity extends AppCompatActivity {
+    private TabLayout tl_favourite_tab;
+    private RecyclerView rv_favouriteItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_favourite);
+
+        tl_favourite_tab = findViewById(R.id.tl_favorite_tab);
+        rv_favouriteItem = findViewById(R.id.rv_favourite_item);
+
+        tl_favourite_tab.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                //TODO display item
+                if (tab.getText().equals("All")) {
+
+                } else if (tab.getText().equals("Do")) {
+
+                } else if (tab.getText().equals("Stay")) {
+
+                } else if (tab.getText().equals("Eat")) {
+
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
     }
 }
--- app/src/main/java/com/t2xm/application/fragment/ProfileFragment.java
+++ app/src/main/java/com/t2xm/application/fragment/ProfileFragment.java
@@ -1,20 +1,36 @@
 package com.t2xm.application.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.t2xm.R;
+import com.t2xm.application.activity.MyFavouriteActivity;
 
 public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button btn_myFavourite = view.findViewById(R.id.btn_my_favourite);
+        btn_myFavourite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity().getApplicationContext(), MyFavouriteActivity.class));
+            }
+        });
     }
 }
