ae64b7c7253ad13a4f0d804c8a692eff1c5c734a
gq <swe1904862@xmu.edu.my>
2021-12-25 19:09:33 +0800

    display more information when click on button why visit xiamen

--- app/src/main/java/com/t2xm/application/fragment/HomeFragment.java
+++ app/src/main/java/com/t2xm/application/fragment/HomeFragment.java
@@ -1,30 +1,31 @@
 package com.t2xm.application.fragment;
 
+import android.app.AlertDialog;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.t2xm.R;
-import com.t2xm.dao.ItemDao;
-import com.t2xm.dao.ReviewDao;
-import com.t2xm.entity.Item;
-import com.t2xm.entity.Review;
-import com.t2xm.utils.adapter.HomeReviewAdapter;
-import com.t2xm.utils.adapter.TopPlacesAdapter;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
+
+    private List<Button> btnList_whyVisitXiamen;
+    private List<String> resource_whyVisitXiamenLong;
+
+    private AlertDialog.Builder builder;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -35,14 +36,30 @@ public class HomeFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        builder = new AlertDialog.Builder(getActivity());
+
+        btnList_whyVisitXiamen = new ArrayList<>();
+        btnList_whyVisitXiamen.add(view.findViewById(R.id.btn_why_visit_xiamen_1));
+        btnList_whyVisitXiamen.add(view.findViewById(R.id.btn_why_visit_xiamen_2));
+        btnList_whyVisitXiamen.add(view.findViewById(R.id.btn_why_visit_xiamen_3));
+        Integer resourceId = getResources().getIdentifier("why_visit_long_arr", "array", getActivity().getPackageName());
+        resource_whyVisitXiamenLong = Arrays.asList(getResources().getStringArray(resourceId));
+
+        for(int i = 0 ; i < btnList_whyVisitXiamen.size() ; i++) {
+            int finalI = i;
+            btnList_whyVisitXiamen.get(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    builder.setMessage(resource_whyVisitXiamenLong.get(finalI)).setPositiveButton("OK", null);
+                    builder.create().show();
+                }
+            });
+        }
+
         //TODO ViewPager
         ImageView iv_homeAnimation = view.findViewById(R.id.iv_home_animation);
         iv_homeAnimation.setBackgroundResource(R.drawable.animation_list);
-
-        // Get the background, which has been compiled to an AnimationDrawable object.
         AnimationDrawable frameAnimation = (AnimationDrawable) iv_homeAnimation.getBackground();
-
-        // Start the animation (looped playback by default).
         frameAnimation.start();
     }
 }
