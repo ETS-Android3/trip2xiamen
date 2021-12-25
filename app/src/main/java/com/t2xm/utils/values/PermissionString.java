f4f1172e31e2c43109a97c4b9029ca3e43f05410
gq <swe1904862@xmu.edu.my>
2021-12-26 01:56:30 +0800

    complete display map location

--- app/src/main/java/com/t2xm/utils/values/PermissionString.java
+++ app/src/main/java/com/t2xm/utils/values/PermissionString.java
@@ -6,6 +6,4 @@ public class PermissionString {
     public static final String CAMERA = Manifest.permission.CAMERA;
     public static final String READ_EXTERNAL_STORAGE = Manifest.permission.READ_EXTERNAL_STORAGE;
     public static final String WRITE_EXTERNAL_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
-    public static final String ACCESS_COARSE_LOCATION  = Manifest.permission.ACCESS_COARSE_LOCATION;
-    public static final String ACCESS_FINE_LOCATION  = Manifest.permission.ACCESS_FINE_LOCATION;
 }
