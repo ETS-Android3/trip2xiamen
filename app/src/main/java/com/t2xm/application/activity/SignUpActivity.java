df418e64c39a09774a01c6e9b1626d8eab73518e
gq <swe1904862@xmu.edu.my>
2021-12-28 17:55:54 +0800

    add error to signup activity

--- app/src/main/java/com/t2xm/application/activity/SignUpActivity.java
+++ app/src/main/java/com/t2xm/application/activity/SignUpActivity.java
@@ -2,23 +2,24 @@ package com.t2xm.application.activity;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.Editable;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 import com.t2xm.R;
 import com.t2xm.dao.UserDao;
 import com.t2xm.entity.User;
 import com.t2xm.utils.ToastUtil;
 import com.t2xm.utils.ValidationUtil;
 
-import org.w3c.dom.Text;
-
 public class SignUpActivity extends AppCompatActivity {
+
+    private TextInputLayout lay_username;
+    private TextInputLayout lay_email;
+    private TextInputLayout lay_password;
+    private TextInputLayout lay_confirmPassword;
 
     private TextInputEditText et_username;
     private TextInputEditText et_email;
@@ -40,44 +41,69 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private boolean validateSignUpInput() {
-        //TODO provide errors
-        if(username.equals("")) {
-            ToastUtil.createAndShowToast(getApplicationContext(), "Please provide your username");
-            return false;
+        boolean valid = true;
+
+        // validate username
+        if (username.equals("")) {
+            lay_username.setError(getString(R.string.error_username_empty));
+            valid = valid && false;
         }
-        if(email.equals("")) {
-            ToastUtil.createAndShowToast(getApplicationContext(), "Please provide your email");
-            return false;
+        else if (ValidationUtil.validateUsername(username) == false) {
+            lay_username.setError(getString(R.string.username_format));
+            valid = valid && false;
         }
-        if(password.equals("")) {
-            ToastUtil.createAndShowToast(getApplicationContext(), "Please provide your password");
-            return false;
+        else if (UserDao.checkUsernameExistence(username) == true) {
+            lay_username.setError(getString(R.string.error_username_exist));
+            valid = valid && false;
         }
-        if(confirmPassword.equals("")) {
-            ToastUtil.createAndShowToast(getApplicationContext(), "Please confirm your password");
-            return false;
+        else {
+            lay_username.setError(getString(R.string.no_error));
         }
-        if (ValidationUtil.validateUsername(username) == false) {
-            ToastUtil.createAndShowToast(getApplicationContext(), "Please provide valid username");
-            return false;
+
+        //validate email
+        if (email.equals("")) {
+            lay_email.setError(getString(R.string.error_email_empty));
+            valid = valid && false;
         }
-        if(ValidationUtil.validateEmail(email) == false) {
-            ToastUtil.createAndShowToast(getApplicationContext(), "Please provide valid email");
-            return false;
+        else if (ValidationUtil.validateEmail(email) == false) {
+            lay_email.setError(getString(R.string.email_format));
+            valid = valid && false;
         }
-        if(ValidationUtil.validatePassword(password) == false) {
-            ToastUtil.createAndShowToast(getApplicationContext(), "Please provide valid password");
-            return false;
+        else if (UserDao.checkEmailExistence(email) == true) {
+            lay_email.setError(getString(R.string.error_email_exist));
+            valid = valid && false;
         }
-        if(UserDao.checkUsernameExistence(username) == true) {
-            ToastUtil.createAndShowToast(getApplicationContext(), "An account with the provided username already exist, please try other username");
-            return false;
+        else {
+            lay_email.setError(getString(R.string.no_error));
         }
-        if(UserDao.checkEmailExistence(email) == true) {
-            ToastUtil.createAndShowToast(getApplicationContext(), "An account with the provided email already exist, please try other email");
-            return false;
+
+        //validate password
+        if (password.equals("")) {
+            lay_password.setError(getString(R.string.error_password_empty));
+            valid = valid && false;
         }
-        return true;
+        else if (ValidationUtil.validatePassword(password) == false) {
+            lay_password.setError(getString(R.string.password_format));
+            valid = valid && false;
+        }
+        else {
+            lay_password.setError(getString(R.string.no_error));
+        }
+
+        //validate confirm password
+        if (confirmPassword.equals("")) {
+            lay_confirmPassword.setError(getString(R.string.error_confirm_password_empty));
+            valid = valid && false;
+        }
+        else if(!confirmPassword.equals(password)) {
+            lay_confirmPassword.setError(getString(R.string.error_password_not_match));
+            valid = valid && false;
+        }
+        else {
+            lay_confirmPassword.setError(getString(R.string.no_error));
+        }
+
+        return valid;
     }
 
     private void updateEditTextValues() {
@@ -88,6 +114,11 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     private void setupActivityComponents() {
+        lay_username = findViewById(R.id.lay_username);
+        lay_email = findViewById(R.id.lay_email);
+        lay_password = findViewById(R.id.lay_password);
+        lay_confirmPassword = findViewById(R.id.lay_confirm_password);
+
         et_username = findViewById(R.id.et_username);
         et_email = findViewById(R.id.et_email);
         et_password = findViewById(R.id.et_password);
@@ -99,14 +130,13 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 updateEditTextValues();
-                if(validateSignUpInput() == true) {
-                    if(password.equals(confirmPassword) == false) {
+                if (validateSignUpInput() == true) {
+                    if (password.equals(confirmPassword) == false) {
                         ToastUtil.createAndShowToast(getApplicationContext(), "The provided password and confirm password do not match");
-                    }
-                    else {
+                    } else {
                         User user = new User(null, username, email, password, null);
-                        boolean result = UserDao.insertUser(user) ;
-                        if(result == true) {
+                        boolean result = UserDao.insertUser(user);
+                        if (result == true) {
                             ToastUtil.createAndShowToast(getApplicationContext(), "Sign up successful");
                             new Handler().postDelayed(new Runnable() {
                                 @Override
@@ -115,8 +145,7 @@ public class SignUpActivity extends AppCompatActivity {
                                     finish();
                                 }
                             }, 1000);
-                        }
-                        else {
+                        } else {
                             ToastUtil.createAndShowToast(getApplicationContext(), "Sign up failed");
                         }
                     }
