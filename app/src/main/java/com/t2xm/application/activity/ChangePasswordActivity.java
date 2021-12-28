3f3f122629047ed4051b767100dadd9b6cc3966e
gq <swe1904862@xmu.edu.my>
2021-12-28 13:18:26 +0800

    complete change password function

--- app/src/main/java/com/t2xm/application/activity/ChangePasswordActivity.java
+++ app/src/main/java/com/t2xm/application/activity/ChangePasswordActivity.java
@@ -1,8 +1,8 @@
 package com.t2xm.application.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +14,8 @@ import com.t2xm.utils.ToastUtil;
 import com.t2xm.utils.ValidationUtil;
 
 public class ChangePasswordActivity extends AppCompatActivity {
+
+    private Activity activity;
 
     private String username;
 
@@ -30,29 +32,28 @@ public class ChangePasswordActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
 
-        username = SharedPreferenceUtil.getUsername(getApplicationContext());
+        activity = this;
+
+        username = SharedPreferenceUtil.getUsername(activity);
 
         setupActivityComponents();
         setupActivityListeners();
     }
 
     private boolean validatePassword() {
-        if(ValidationUtil.validatePassword(newPassword)) {
-            return newPassword.equals(confirmPassword);
-        }
-        return false;
+        return ValidationUtil.validatePassword(newPassword);
     }
 
     private void updateInputFields() {
-        currentPassword = String.valueOf(et_currentPassword.getText());
-        newPassword = String.valueOf(et_newPassword.getText());
-        confirmPassword = String.valueOf(et_confirmPassword.getText());
+        currentPassword = String.valueOf(et_currentPassword.getText()).trim();
+        newPassword = String.valueOf(et_newPassword.getText()).trim();
+        confirmPassword = String.valueOf(et_confirmPassword.getText()).trim();
     }
 
     private void setupActivityComponents() {
         et_currentPassword = findViewById(R.id.et_current_password);
         et_newPassword = findViewById(R.id.et_new_password);
-        et_confirmPassword = findViewById(R.id.et_new_password);
+        et_confirmPassword = findViewById(R.id.et_confirm_password);
     }
 
     private void setupActivityListeners() {
@@ -60,17 +61,29 @@ public class ChangePasswordActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 updateInputFields();
-                //TODO test
-                if(currentPassword.equals(UserDao.getUserPasswordByUsername(username))) {
-                    boolean result = false;
-                    if(validatePassword()) {
-                        result = UserDao.editUserEmailByUsername(username, newPassword);
+                if (currentPassword.equals("") || newPassword.equals("") || confirmPassword.equals("")) {
+                    ToastUtil.createAndShowToast(getApplicationContext(), "Please fill up all fields");
+                    return;
+                }
+
+                if (currentPassword.equals(newPassword)) {
+                    ToastUtil.createAndShowToast(getApplicationContext(), "Current password and new password cannot be " +
+                            "the same");
+                } else if (validatePassword() == true) {
+                    if(newPassword.equals(confirmPassword)) {
+                        if (UserDao.editUserPasswordByUsername(username, newPassword)) {
+                            ToastUtil.createAndShowToast(getApplicationContext(), "Your password has been updated");
+                            onBackPressed();
+                            finish();
+                        } else {
+                            ToastUtil.createAndShowToast(getApplicationContext(), "Error: Please try again");
+                        }
                     }
-                    if(result == true) {
-                        ToastUtil.createAndShowToast(getApplicationContext(), "Your password has been changed");
-                        onBackPressed();
-                        finish();
+                    else {
+                        ToastUtil.createAndShowToast(activity, "New Password and Confirm Password do not match");
                     }
+                } else {
+                    ToastUtil.createAndShowToast(activity, "Please provide valid password");
                 }
             }
         });
