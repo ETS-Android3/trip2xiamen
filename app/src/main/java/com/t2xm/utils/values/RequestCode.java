27eefb6d93b39d63ca59124b3e68d5432aedb6ad
gq <swe1904862@xmu.edu.my>
2021-12-29 09:35:57 +0800

    add request code for intent startActivityForResult

--- app/src/main/java/com/t2xm/utils/values/RequestCode.java
+++ app/src/main/java/com/t2xm/utils/values/RequestCode.java
@@ -11,6 +11,14 @@ public class RequestCode {
     public static final Integer PICK_IMAGE_FROM_GALLERY = 31;
     public static final Integer SNAP_PHOTO_FROM_CAMERA = 32;
 
+    public static final Integer WRITE_REVIEW = 51;
+    public static final Integer CHANGE_PASSWORD = 52;
+    public static final Integer CHANGE_EMAIL = 53;
+    public static final Integer VIEW_FAVOURITE_ITEM = 54;
+    public static final Integer VIEW_REVIEWED_ITEM = 55;
+    public static final Integer START_SETTINGS = 56;
+
+
     //Permission
     public static final Integer USE_CAMERA_PERMISSION = 101;
     public static final Integer READ_EXTERNAL_STORAGE_PERMISSION = 102;
